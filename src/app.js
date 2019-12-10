e3648af28618415f51e6d6c7eb0edf24795779ea
Hallan Santos <hallanp@inatel.br>
2019-12-10 13:23:56 -0300

    Add usefull middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
-const taskRoutes = require('./routes/tasks');
-app.use('/tasks', taskRoutes);
-
+// Middlewares
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const notFound = require('./middleware/not-found');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Routes
+const taskRouter = require('./routes/tasks');
+app.use('/tasks', taskRouter);
+
 app.use(notFound);
 
 module.exports = app;
